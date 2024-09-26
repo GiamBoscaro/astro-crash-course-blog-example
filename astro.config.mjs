a967616ab55d57312e35c4902075c87498738c59
Giammarco Boscaro <giammarco.boscaro@gmail.com>
2024-09-26 20:52:17 +0200

    Added SSR config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  output: 'server',
 });
